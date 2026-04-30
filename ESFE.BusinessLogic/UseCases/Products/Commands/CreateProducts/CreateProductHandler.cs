5be161d2a37d0a046e3fc9ad469d937da3f7944f
Gabriel <gabrielobandogarcia100@gmail.com>
2026-04-29 20:14:12 -0600

    feat(products): agregar estructura base de casos de uso para productos

---
+++ ESFE.BusinessLogic/UseCases/Products/Commands/CreateProducts/CreateProductHandler.cs
@@ -0,0 +1,10 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ESFE.BusinessLogic.UseCases.Products.Commands.CreateProducts
+{
+    internal class CreateProductHandler
+    {
+    }
+}
