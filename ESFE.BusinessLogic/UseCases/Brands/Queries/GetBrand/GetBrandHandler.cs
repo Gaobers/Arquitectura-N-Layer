d8c55d0bdb57633d389d26e96df6cc815f11b76b
Gabriel <gabrielobandogarcia100@gmail.com>
2026-04-29 20:27:18 -0600

    feat(brands): agregar caso de uso para obtener una marca por id
    - Se implementa GetBrandQuery utilizando MediatR
    - Se crea GetBrandHandler para manejar la consulta de una marca
    - Se obtiene la entidad desde el repositorio por su identificador
    - Se valida si la marca existe
    - Se utiliza Mapster para mapear la entidad Brand a BrandResponse
    - Se retorna la marca obtenida o un objeto vacío si no existe

---
+++ ESFE.BusinessLogic/UseCases/Brands/Queries/GetBrand/GetBrandHandler.cs
@@ -0,0 +1,22 @@
+﻿using ESFE.BusinessLogic.DTOs;
+using ESFE.DataAccess.Interfaces;
+using ESFE.Entities;
+using Mapster;
+using MediatR;
+
+namespace ESFE.BusinessLogic.UseCases.Brands.Queries.GetBrand;
+
+internal sealed class GetBrandHandler(IEfRepository<Brand> _repository) : IRequestHandler<GetBrandQuery, BrandResponse>
+{
+    public async Task<BrandResponse> Handle(GetBrandQuery query, CancellationToken cancellationToken)
+    {
+        var brand = await _repository.GetByIdAsync(query.brandId, cancellationToken);
+
+        if (brand == null)
+        {
+            return new BrandResponse();
+        }
+
+        return brand.Adapt<BrandResponse>();
+    }
+}
