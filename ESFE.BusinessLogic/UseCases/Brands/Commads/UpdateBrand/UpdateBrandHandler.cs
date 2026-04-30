fce90b8b7abf54cd59aaa775cba97315c14e681c
Gabriel <gabrielobandogarcia100@gmail.com>
2026-04-29 19:31:31 -0600

    * feat(brands): agregar caso de uso para actualizar marca
    - Se implementa UpdateBrandCommand utilizando MediatR
    - Se crea UpdateBrandHandler para manejar la lógica de actualización
    - Se obtiene la entidad existente desde el repositorio
    - Se utiliza Mapster para mapear UpdateBrandRequest sobre la entidad existente
    - Se actualiza la entidad mediante IEfRepository
    - Se retorna el identificador de la marca actualizada

---
+++ ESFE.BusinessLogic/UseCases/Brands/Commads/UpdateBrand/UpdateBrandHandler.cs
@@ -0,0 +1,30 @@
+﻿using ESFE.DataAccess.Interfaces;
+using ESFE.Entities;
+using Mapster;
+using MediatR;
+
+namespace ESFE.BusinessLogic.UseCases.Brands.Commads.UpdateBrand;
+
+internal sealed class UpdateBrandHandler(IEfRepository<Brand> _repository) : IRequestHandler<UpdateBrandCommand, int>
+{
+    public async Task<int> Handle(UpdateBrandCommand command, CancellationToken cancellationToken)
+    {
+        try
+        {
+            var existingBrand = await _repository.GetByIdAsync(command.Request.BrandId);
+
+            if (existingBrand is null) return 0;
+
+            existingBrand = command.Request.Adapt(existingBrand);
+
+            await _repository.UpdateAsync(existingBrand, cancellationToken);
+
+            return existingBrand.BrandId;
+        }
+        catch (Exception)
+        {
+            return 0;
+            throw;
+        }
+    }
+}
