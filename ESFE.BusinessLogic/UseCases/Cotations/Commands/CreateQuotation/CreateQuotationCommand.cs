0e3dc1691e97a4676dbda682f220f78c4550b90a
Rosaura <Karlarosauras@gmail.com>
2026-05-01 14:03:07 -0600

    Karla Rosaura Sánchez Hernández feat: implementar comando y manejador para la creación de cotizaciones
    Se crea el comando CreateQuotationCommand basado en el DTO de petición.
    
    Se implementa CreateQuotationHandler con soporte para transacciones (Begin, Commit, Rollback).
    
    Se integra Mapster para la adaptación automática de CreateQuotationRequest a la entidad Quotation.

---
+++ ESFE.BusinessLogic/UseCases/Cotations/Commands/CreateQuotation/CreateQuotationCommand.cs
@@ -0,0 +1,6 @@
+﻿using ESFE.BusinessLogic.DTOs;
+using MediatR;
+
+namespace ESFE.BusinessLogic.UseCases.Cotations.Commands.CreateQuotation;
+
+public record CreateQuotationCommand(CreateQuotationRequest Request) : IRequest<long>;
