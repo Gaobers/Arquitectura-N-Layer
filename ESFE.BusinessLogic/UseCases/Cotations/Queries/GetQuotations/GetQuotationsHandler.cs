6464c256dfdc395cb809b19cebcb65e6682b7d05
Rosaura <Karlarosauras@gmail.com>
2026-05-01 17:24:11 -0600

    Karla Rosaura Sánchez Hernández feat: implementar consulta para listar todas las cotizaciones
    Se crea el registro GetQuotationsQuery para la petición de la lista completa.
    
    Se implementa el manejador GetQuotationsHandler utilizando el repositorio de cotizaciones.
    
    Se añade el mapeo de la colección de entidades Quotation a una lista de QuotationResponse mediante Mapster.

---
+++ ESFE.BusinessLogic/UseCases/Cotations/Queries/GetQuotations/GetQuotationsHandler.cs
@@ -0,0 +1,28 @@
+﻿using ESFE.BusinessLogic.DTOs;
+using ESFE.DataAccess.Interfaces;
+using ESFE.Entities;
+using Mapster;
+using MediatR;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ESFE.BusinessLogic.UseCases.Cotations.Queries.GetQuotations;
+
+internal sealed class GetQuotationsHandler(IEfRepository<Quotation> _Repository)
+    : IRequestHandler<GetQuotationsQuery, List<QuotationResponse>>
+{
+    public async Task<List<QuotationResponse>> Handle(GetQuotationsQuery request, CancellationToken cancellationToken)
+    {
+        var quotations = await _Repository.ListAsync(cancellationToken);
+
+        if (quotations is null || !quotations.Any())
+        {
+
+            return new List<QuotationResponse>();
+        }
+        return quotations.Adapt<List<QuotationResponse>>();
+       
+       
+    }
+}
---
+++ ESFE.BusinessLogic/UseCases/Cotations/Queries/GetQuotations/GetQuotationsQuery.cs
@@ -0,0 +1,10 @@
+﻿using ESFE.BusinessLogic.DTOs;
+using MediatR;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ESFE.BusinessLogic.UseCases.Cotations.Queries.GetQuotations;
+
+public record GetQuotationsQuery : IRequest<List<QuotationResponse>>;
+
