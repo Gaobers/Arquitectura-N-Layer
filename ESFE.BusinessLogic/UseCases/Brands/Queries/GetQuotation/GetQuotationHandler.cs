72dbc9c03af085de8b89c6adec170e1fa84eaf79
Rosaura <Karlarosauras@gmail.com>
2026-05-01 14:56:42 -0600

    Karla Rosaura Sánchez hernández feat: implementar consulta de cotización por ID
    Se crea el registro GetQuotationQuery para la búsqueda por identificador.
    
    Se implementa el manejador GetQuotationHandler utilizando el repositorio de cotizaciones.
    
    Se integra el mapeo automático a QuotationResponse mediante Mapster.

---
+++ ESFE.BusinessLogic/UseCases/Brands/Queries/GetQuotation/GetQuotationHandler.cs
@@ -0,0 +1,25 @@
+﻿using ESFE.BusinessLogic.DTOs;
+using ESFE.DataAccess.Interfaces;
+using ESFE.Entities;
+using Mapster;
+using MediatR;
+
+namespace ESFE.BusinessLogic.UseCases.Brands.Queries.GetQuotation;
+
+internal sealed class GetQuotationHandler(IEfRepository<Quotation> _repository)
+: IRequestHandler<GetQuotationQuery, QuotationResponse>
+{
+    public async Task<QuotationResponse> Handle(GetQuotationQuery query, CancellationToken cancellationToken)
+    {
+        var quotation = await _repository.GetByIdAsync(query.QuotationId, cancellationToken);
+
+        if (quotation is null)
+        {
+            return new QuotationResponse();
+        }
+
+        return quotation.Adapt<QuotationResponse>();
+    }
+}
+
+
