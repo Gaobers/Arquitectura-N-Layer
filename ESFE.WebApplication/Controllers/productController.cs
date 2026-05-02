0be30ad4f97f721fd963b08d4c0c0c92ac4f2c23
Andrea <denissedepaz16@gmail.com>
2026-05-01 18:15:12 -0600

    Cambios del controlador producto

--- ESFE.WebApplication/Controllers/productController.cs
+++ ESFE.WebApplication/Controllers/productController.cs
@@ -1,154 +1,108 @@
-﻿using ESFE.Entities;
+﻿using ESFE.BusinessLogic.DTOs;
+using ESFE.BusinessLogic.UseCases.Brands.Queries.GetBrands;
+using ESFE.BusinessLogic.UseCases.Products.Commands.CreateProduct;
+using ESFE.BusinessLogic.UseCases.Products.Commands.UpdateProduct;
+using ESFE.BusinessLogic.UseCases.Products.Commands.DeleteProduct; // Asegúrate de tener este
+using ESFE.BusinessLogic.UseCases.Products.Queries.GetProduct;
+using ESFE.BusinessLogic.UseCases.Products.Queries.GetProducts;
+using Mapster;
+using MediatR;
+using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.Rendering;
-using Microsoft.EntityFrameworkCore;
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
-using TuProyecto.Data; // Cambia esto al espacio de nombres de tu carpeta Data
-using TuProyecto.Models; // Cambia esto al espacio de nombres de tu carpeta Models
 
-namespace TuProyecto.Controllers
+namespace ESFE.WebApplication.Controllers
 {
+    [Authorize]
     public class ProductsController : Controller
     {
-        private readonly ApplicationDbContext _context;
+        private readonly IMediator _mediator;
 
-        public ProductsController(ApplicationDbContext context)
+        public ProductsController(IMediator mediator)
         {
-            _context = context;
+            _mediator = mediator;
         }
 
-        // GET: Products
         public async Task<IActionResult> Index()
         {
-            return View(await _context.Products.ToListAsync());
+            var products = await _mediator.Send(new GetProductsQuery());
+            return View(products);
         }
 
-        // GET: Products/Details/5
-        public async Task<IActionResult> Details(int? id)
+        public async Task<IActionResult> Details(int id)
         {
-            if (id == null)
-            {
-                return NotFound();
-            }
-
-            var product = await _context.Products
-                .FirstOrDefaultAsync(m => m.Id == id);
-            if (product == null)
-            {
-                return NotFound();
-            }
-
+            var product = await _mediator.Send(new GetProductQuery { Id = id });
+            if (product == null) return NotFound();
             return View(product);
         }
 
-        // GET: Products/Create
-        public IActionResult Create()
+        public async Task<IActionResult> Create()
         {
+            // Para el Select de Marcas en la vista de creación
+            var brands = await _mediator.Send(new GetBrandsQuery());
+            ViewBag.Brands = new SelectList(brands, "Id", "Name");
             return View();
         }
 
-        // POST: Products/Create
         [HttpPost]
         [ValidateAntiForgeryToken]
-        public async Task<IActionResult> Create([Bind("Id,Name,Price,Description")] Product product)
+        public async Task<IActionResult> Create(CreateProductDTO createProductDTO)
         {
-            if (ModelState.IsValid)
+            try
             {
-                _context.Add(product);
-                await _context.SaveChangesAsync();
-                return RedirectToAction(nameof(Index));
+                var result = await _mediator.Send(createProductDTO.Adapt<CreateProductCommand>());
+                if (result > 0) return RedirectToAction(nameof(Index));
+
+                return View(createProductDTO);
             }
-            return View(product);
+            catch (Exception ex)
+            {
+                ModelState.AddModelError("", ex.Message);
+                return View(createProductDTO);
+            }
         }
 
-        // GET: Products/Edit/5
-        public async Task<IActionResult> Edit(int? id)
+        public async Task<IActionResult> Edit(int id)
         {
-            if (id == null)
-            {
-                return NotFound();
-            }
+            var product = await _mediator.Send(new GetProductQuery { Id = id });
+            if (product == null) return NotFound();
 
-            var product = await _context.Products.FindAsync(id);
-            if (product == null)
-            {
-                return NotFound();
-            }
-            return View(product);
+            var brands = await _mediator.Send(new GetBrandsQuery());
+            ViewBag.Brands = new SelectList(brands, "Id", "Name");
+
+            return View(product.Adapt<UpdateProductDTO>());
         }
 
-        // POST: Products/Edit/5
         [HttpPost]
         [ValidateAntiForgeryToken]
-        public async Task<IActionResult> Edit(int id, [Bind("Id,Name,Price,Description")] Product product)
+        public async Task<IActionResult> Edit(UpdateProductDTO updateProductDTO)
         {
-            if (id != product.Id)
+            try
             {
-                return NotFound();
-            }
+                var result = await _mediator.Send(updateProductDTO.Adapt<UpdateProductCommand>());
+                if (result > 0) return RedirectToAction(nameof(Index));
 
-            if (ModelState.IsValid)
+                return View(updateProductDTO);
+            }
+            catch (Exception ex)
             {
-                try
-                {
-                    _context.Update(product);
-                    await _context.SaveChangesAsync();
-                }
-                catch (DbUpdateConcurrencyException)
-                {
-                    if (!ProductExists(product.Id))
-                    {
-                        return NotFound();
-                    }
-                    else
-                    {
-                        throw;
-                    }
-                }
+                ModelState.AddModelError("", ex.Message);
+                return View(updateProductDTO);
+            }
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> Delete(int id)
+        {
+            try
+            {
+                await _mediator.Send(new DeleteProductCommand { Id = id });
                 return RedirectToAction(nameof(Index));
             }
-            return View(product);
-        }
-
-        // GET: Products/Delete/5
-        public async Task<IActionResult> Delete(int? id)
-        {
-            if (id == null)
+            catch
             {
-                return NotFound();
+                return RedirectToAction(nameof(Index));
             }
-
-            var product = await _context.Products
-                .FirstOrDefaultAsync(m => m.Id == id);
-            if (product == null)
-            {
-                return NotFound();
-            }
-
-            return View(product);
-        }
-
-        // POST: Products/Delete/5
-        [HttpPost, ActionName("Delete")]
-        [ValidateAntiForgeryToken]
-        public async Task<IActionResult> DeleteConfirmed(int id)
-        {
-            var product = await _context.Products.FindAsync(id);
-            if (product != null)
-            {
-                _context.Products.Remove(product);
-            }
-
-            await _context.SaveChangesAsync();
-            return RedirectToAction(nameof(Index));
-        }
-
-        private bool ProductExists(int id)
-        {
-            return _context.Products.Any(e => e.Id == id);
         }
     }
 }
